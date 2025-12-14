8043199b9669786d2d2230020ee12955a0b78a54
Jiri-Blazek <blazek.j@centrum.cz>
2025-12-14 15:23:21 +0100

    ukol

--- 40/test_pillow_ukol.py
+++ 40/test_pillow_ukol.py
@@ -12,27 +12,22 @@ def resize_image(path, save_to, size=(320, 320)):
 
 def get_thumb_path(path, size=(320, 320)):
     comma_position = path.rindex(".")
-    extra_name = "-" + str(size[0] + "x" + size[1])
+    extra_name = "-" + str(size[0])
     new_path = path[:comma_position] + extra_name + path[comma_position:]
     return new_path
 
 
-if __name__ == "__main__":
+path = Path(
+    r"C:\Users\blaze\OneDrive\Plocha\Programovani\Github\itstep-ukoly\40\obrazky"
+)
 
-
-
-folder = Path("cesta/k/slozce")
-
-for file in folder.iterdir():
+for file in path.iterdir():
     if file.is_file():
-        print(file)
 
-
-
-    def main():
-        path = r"C:\Users\blaze\OneDrive\Plocha\Programovani\Github\itstep-ukoly\40\>"
+        path = str(file)
+        print(path)
         save_to = get_thumb_path(path)
         resize_image(path, save_to)
 
-    main()
+
 # nacist obrazky a doplnit do nich do nazvu velikost
